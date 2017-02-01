75cbd7c12e3a6632ae537f9982b4043431d1fdff
3846masa <3846masahiro+git@gmail.com>
2017-02-02 02:03:49 +0900

    device-battery

---
+++ device-battery/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ENV = process.env;
+
+let batteryStatus = {};
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'ok',
+    data: batteryStatus
+  });
+});
+
+app.post('/', (req, res) => {
+  if (!('key' in req.query) || req.query.key !== (ENV['SECRET_KEY'] || '')) {
+    throw new Error('Invalid key.');
+  }
+  if (!req.body) {
+    throw new Error('Invalid data.');
+  }
+  batteryStatus = req.body;
+  res.json({
+    status: 'ok'
+  });
+});
+
+app.use((_err, req, res, next) => {
+  res.status(400).json({
+    status: 'error',
+    data: _err.message
+  });
+  next();
+});
+
+app.listen(ENV['PORT'] || 3000, () => {
+  console.log('Listening...');
+});
